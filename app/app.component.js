3276ffe660d7dc78579256ece59b290b7c2d5ff3
mariusz <mariuszjurczenko@gmail.com>
2018-12-18 11:27:07 +0100

    Angular2 odc9

--- app/app.component.js
+++ app/app.component.js
@@ -1,13 +1,12 @@
-(function(app){
-  var Component = ng.core.Component;
+'use strict';
 
-app.AppComponent = Component({
-    selector: 'my-app',
-    template: `<h1>Angular 2.0. Hello World!!!</h1>
-              <second></second>`
-})
-.Class({
-    constructor : function AppComponent (){}
-});
+(function (app) {
+    var Component = ng.core.Component;
 
+    app.AppComponent = Component({
+        selector: 'my-app',
+        template: '<h1>Angular 2.0. Hello World!!!</h1>\n              <second></second>'
+    }).Class({
+        constructor: function AppComponent() {}
+    });
 })(window.app || (window.app = {}));
