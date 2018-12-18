5bb9a6fa079b7ed33b22af9b3f4413392875cdf0
mariusz <mariuszjurczenko@gmail.com>
2018-12-18 10:57:09 +0100

    Angular2 odc5

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
-(function() {
+(function(){
 
   var Component = ng.core.Component;
   var NgModule = ng.core.NgModule;
   var BrowserModule = ng.platformBrowser.BrowserModule;
   var platformBrowserDynamic = ng.platformBrowserDynamic.platformBrowserDynamic;
+  var Class = ng.core.Class;
 
+var QuoteService = Class({
+    constructor: function QuoteService (){
+        this.quotes = quotes2;
+    },
 
-  var SecondComponent = Component({
-    selector: 'second',
-    template: '<p><em>{{quote.line}}</em> - {{quote.author}}</p>'
-  })
-  .Class({
-    constructor: function() {
-      var randomIndex = Math.floor(Math.random() * quotes.length);
-      this.quote = quotes[randomIndex];
+    getRandomQuote: function(){
+        var randomIndex = Math.floor(Math.random() * this.quotes.length);
+        return this.quotes[randomIndex];
     }
-  });
+})  
 
+var SecondComponent = Component({
+    selector:'second',
+    template:'<p><em>{{quote.line}}</em>{{quote.author}}</p>'
+})
+.Class({
+    constructor: [QuoteService, function SecondComponent (quoteService){
+        this.quote = quoteService.getRandomQuote();
+    }]
+});
 
-  var AppComponent = Component({
+var AppComponent = Component({
     selector: 'my-app',
-    template: '<h1>Angular 2. Hello World!!!</h1>' +
-              '<second></second>'
-  })
-  .Class({
-    constructor: function() { }
-  });
+    template: `<h1>Angular 2.0. Hello World!!!</h1>
+              <second></second>
+              <second></second>`
+})
+.Class({
+    constructor : function AppComponent (){}
+});
 
-  
-  var AppModule = NgModule({
-    imports: [ng.platformBrowser.BrowserModule],
-    declarations: [AppComponent, SecondComponent],
+
+var AppModule = NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent, SecondComponent], 
+    providers:[QuoteService],  
     bootstrap: [AppComponent]
-  })
-  .Class({
-    constructor: function() { }
-  });
+})
+.Class({
+    constructor: function(){}
+})
 
-  
-  platformBrowserDynamic()
-    .bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule);
 
-      var quotes = [
+ var quotes2 = [
     {
       "line": "Walking on water and developing software from a specification are easy if both are frozen.",
       "author": "Edward V Berard"
@@ -63,5 +72,6 @@
       "author": "Tom Cargill"
     }
   ];
+
 
 })();
