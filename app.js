2eee1e7ac350032daa726590515ab7c4e9dba38e
mariusz <mariuszjurczenko@gmail.com>
2018-12-18 10:55:08 +0100

    Angular2 odc4

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 (function() {
 
-  var AppComponent = ng.core.Component({
+  var Component = ng.core.Component;
+  var NgModule = ng.core.NgModule;
+  var BrowserModule = ng.platformBrowser.BrowserModule;
+  var platformBrowserDynamic = ng.platformBrowserDynamic.platformBrowserDynamic;
+
+
+  var SecondComponent = Component({
+    selector: 'second',
+    template: '<p><em>{{quote.line}}</em> - {{quote.author}}</p>'
+  })
+  .Class({
+    constructor: function() {
+      var randomIndex = Math.floor(Math.random() * quotes.length);
+      this.quote = quotes[randomIndex];
+    }
+  });
+
+
+  var AppComponent = Component({
     selector: 'my-app',
-    template: '<h1>Angular 2. Hello World!!!</h1>'
+    template: '<h1>Angular 2. Hello World!!!</h1>' +
+              '<second></second>'
   })
   .Class({
     constructor: function() { }
   });
 
   
-  var AppModule = ng.core.NgModule({
+  var AppModule = NgModule({
     imports: [ng.platformBrowser.BrowserModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent, SecondComponent],
     bootstrap: [AppComponent]
   })
   .Class({
@@ -19,7 +38,30 @@
   });
 
   
-  ng.platformBrowserDynamic.platformBrowserDynamic()
+  platformBrowserDynamic()
     .bootstrapModule(AppModule);
+
+      var quotes = [
+    {
+      "line": "Walking on water and developing software from a specification are easy if both are frozen.",
+      "author": "Edward V Berard"
+    },
+    {
+      "line": "It always takes longer than you expect, even when you take into account Hofstadter's Law.",
+      "author": "Hofstadter's Law"
+    },
+    {
+      "line": "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
+      "author": "Bill Gates"
+    },
+    {
+      "line": "If debugging is the process of removing software bugs, then programming must be the process of putting them in.",
+      "author": "Edsger Dijkstra"
+    },
+    {
+      "line": "The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.",
+      "author": "Tom Cargill"
+    }
+  ];
 
 })();
